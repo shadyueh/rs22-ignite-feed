e225c7ac0142ed4ca3f3c24c84e292ba4cbf929b
Held Grijo <held.grijo@gmail.com>
2022-06-22 07:20:02 -0400

    feat: components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-function App() {
-  return <h1>Hello Ignite</h1>;
-}
+import { Post } from "./Post";
 
-export default App;
+export function App() {
+  return (
+    <div>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+}
