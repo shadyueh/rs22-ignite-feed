8076b08ac291225fb745ae4fd192d19eb7cdeae5
Held Grijo <held.grijo@manaus.am.gov.br>
2022-06-30 10:33:20 -0400

    feat: set Post component propeties

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,23 @@ import { Post } from "./Post";
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Andre Gide"
+        content="It is better to be hated for what you are than to be loved for what you are not."
+      />
+      <Post
+        author="John Green"
+        content="The only way out of the labyrinth of suffering is to forgive."
+      />
+      <Post
+        author="W.C. Fields"
+        content="I am free of all prejudice. I hate everyone equally."
+      />
+      <Post
+        author="Pablo Neruda"
+        content="I love you as certain dark things are to be loved,
+        in secret, between the shadow and the soul."
+      />
     </div>
   );
 }
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,3 +1,10 @@
-export function Post() {
-  return <p>Post</p>;
+export function Post(props) {
+  return (
+    <div>
+      <blockquote>
+        <p>&ldquo;{props.content}&rdquo;</p>
+        <small>{props.author}</small>
+      </blockquote>
+    </div>
+  );
 }
