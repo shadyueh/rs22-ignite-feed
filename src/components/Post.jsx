fc5d03d1c16097fe38eb973b7b1aeeacdc5b091e
Held Grijo <held.grijo@gmail.com>
2022-07-04 23:31:25 -0400

    feat: add Avatar component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,15 +1,13 @@
 import styles from "./Post.module.css";
 import { Comment } from "./Comment";
+import { Avatar } from "./Avatar";
 
 export function Post() {
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/shadyueh.png"
-          />
+          <Avatar src="https://github.com/shadyueh.png" />
           <div className={styles.authorInfo}>
             <strong>Shadow Yueh</strong>
             <span>Shadow Bringer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
 import styles from "./Sidebar.module.css";
 
@@ -8,17 +9,10 @@ export function Sidebar() {
       <img
         className={styles.cover}
         src="https://images.pexels.com/photos/270366/pexels-photo-270366.jpeg?cs=srgb&dl=pexels-pixabay-270366.jpg&fm=jpg&h=359&w=500&fit=crop"
-        alt=""
-        srcset=""
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/shadyueh.png"
-          alt=""
-          srcset=""
-        />
+        <Avatar src="https://github.com/shadyueh.png" />
         <strong>Shadow Yueh</strong>
         <span>Shadow Bringer</span>
       </div>
